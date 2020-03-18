59ac28c28982b2a1d05797eac4ce089cf87b87f9
Federico <dm9000@libero.it>
2020-03-18 15:02:17 +0100

    creazione component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GamegameDetailComponent } from './components/gamegame-detail/gamegame-detail.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { EditGameComponent } from './components/edit-game/edit-game.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GameListComponent,
+    GamegameDetailComponent,
+    GameDetailComponent,
+    EditGameComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
